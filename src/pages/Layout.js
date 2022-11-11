bac72d1b30c8ed197d3b5598203228ee4c767c71
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2022-11-11 22:51:55 +0100

    1 file modified

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,38 +3,40 @@ import { Outlet, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 const NavUnlisted = styled.ul`
 
+
   display: flex;
+  float: right;
+
 
   a {
     text-decoration: none;
   }
 
   li {
-    color: green;
-    margin: 0 0.8rem;
-    font-size: 1.3rem;
-    position: relative;
-    list-style: none;
-    border: 2px green;
-     color: white;
+  text-align: center;
+  color: green;
+  margin: 0 0.8rem;
+  font-size: 1.3rem;
+  position: relative;
+  list-style: none;
+  border: 2px green;
+  color: white;
   text-shadow: 2px 2px 4px #000000;
   border-radius: 10px;
   box-shadow: 2px 2px 10px gray;
   padding: 10px;
   }
 
-  .current {
-    li {
-      border-bottom: 2px solid black;
-    }
-  }
+ }
 `;
 
 function Layout() {
   return (
     <NavUnlisted>
       <NavLink to="/" activeClassName="current" exact>
-        <li>Home</li>
+        <li>
+          <span>Home</span>
+        </li>
       </NavLink>
       <NavLink to="/about" activeClassName="current" exact>
         <li>About</li>
